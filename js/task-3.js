75418fb81aca40b69fc23e75c46ae97f4a76d65f
Pasha <artemnko67@gmail.com>
2026-03-12 11:52:46 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+function getElementWidth(content, padding, border) {
+  // Перетворюємо рядки типу "50px" у числа
+  const contentValue = Number.parseFloat(content);
+  const paddingValue = Number.parseFloat(padding);
+  const borderValue = Number.parseFloat(border);
+
+  // Формула загальної ширини: content + 2 * padding + 2 * border
+  const totalWidth = contentValue + 2 * paddingValue + 2 * borderValue;
+  
+  return totalWidth;
+}
+
+// Перевірка роботи функції
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
